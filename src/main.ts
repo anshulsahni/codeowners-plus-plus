ed3a965f2e12a699a533e3ae1b8f1f01dbfb18e8
Anshul Sahni <anshul_sahni@live.com>
2023-08-06 18:32:53 +0530

    testing

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { getInput } from "@actions/core";
 import * as glob from "@actions/glob";
+import * as github from "@actions/github";
 
 async function run(): Promise<void> {
   const branch = getInput("branch");
+  console.log(JSON.stringify(github.context));
 
   const globber = await glob.create("codeowners-plus-plus");
   const files = await globber.glob();
